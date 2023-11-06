936554a0d1a8475260718749e8f63e7e87cab4c1
Farukh <akbfarukh@gmail.com>
2023-11-06 18:34:17 +0600

    mod

--- script.js
+++ script.js
@@ -21,11 +21,13 @@ const model = document.querySelector('#model')
 const manufacturer = document.querySelector('#manufacturer')
 const input = document.querySelector('#input')
 const submit = document.querySelector('#submit')
+const getImg = document.querySelector('#get_img')
+const imgCat = document.querySelector('#cat_img')
 
 submit.addEventListener('click', () => {
     let value = input.value;
     fetch(`https://swapi.dev/api/people/${value}`)
-        .then(response => response.json())
+        .then(res => res.json())
         .then(personData => {
             name.innerHTML = personData.name;
             color.innerHTML = personData.hair_color;
@@ -40,5 +42,12 @@ submit.addEventListener('click', () => {
         })
 })
 
-
-
+const apiToken = 'live_Zc4tnamzIqO5XLpMD8aXqETRSVqpdasICqw0bsSiuJ6Hkh6OKyb3o7mkgTJqonnI'
+getImg.addEventListener('click', () => {
+    fetch('https://api.thecatapi.com/v1/images/search')
+        .then(res => res.json())
+        .then(imgData => {
+            imgUrl = imgData[0].url
+            imgCat.setAttribute('src', imgUrl)
+        })
+})
